08d5dfa43c2158585894acaa083950109bf68204
luohong123 <3300536651@qq.com>
2019-06-20 14:52:05 +0800

    update to angular8

--- src/core/service/message.service.ts
+++
@@ -1,4 +0,0 @@
-export class MessageService {
-    constructor() {
-    }
-}
--- src/feature/budget/components/fcsystem/fcsystem.route.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router'
-import { SysmessagedetailComponent } from './components/sysmessage/sysmessagedetail.component'
-
-export const Routers: Routes = [
-  {
-    path: 'sysmessageDetail',
-    component: SysmessagedetailComponent
-  }
-]
