e923463a8eb353e82b352638825fc8f0aa88cf1e
luohong123 <3300536651@qq.com>
2019-06-20 14:54:50 +0800

    update to angular8

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular8-go!');
+    expect(page.getTitleText()).toEqual('Welcome to angular8-fast-start!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular8-go'),
+      dir: require('path').join(__dirname, './coverage/angular8-fast-start'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
