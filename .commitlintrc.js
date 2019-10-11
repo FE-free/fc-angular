aa95ace30bcf69e99b06f00b4e4269c3c01c81c6
luohong123 <3300536651@qq.com>
2019-10-11 15:09:56 +0800

    :art: style:
    edit gitmoji

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,7 +2,7 @@
  * @Author: honghong
  * @Date: 2019-10-11 11:15:48
  * @LastEditors: honghong
- * @LastEditTime: 2019-10-11 11:16:31
+ * @LastEditTime: 2019-10-11 14:59:52
  * @Description: 校验你的 message,如果我们提交的不符合指向的规范, 直接拒绝提交
  * @email: 3300536651@qq.com
  */
@@ -10,5 +10,9 @@ module.exports = {
   extends: [
     '@commitlint/config-conventional'
   ],
-  rules: {}
+  // 参考链接：https://commitlint.js.org/#/reference-rules
+  rules: {
+    'subject-empty': [0, 'never'], // 
+    'type-empty': [0, 'never']
+  }
 };
--- .cz-config.js
+++ .cz-config.js
@@ -2,7 +2,7 @@
  * @Author: honghong
  * @Date: 2019-10-11 09:50:12
  * @LastEditors: honghong
- * @LastEditTime: 2019-10-11 14:42:47
+ * @LastEditTime: 2019-10-11 15:08:52
  * @Description:  不使用cz-conventional-changelog (一个符合 Angular团队规范的 preset)
  * 自定义格式
  * @email: 3300536651@qq.com
@@ -23,32 +23,39 @@ module.exports = {
       name: 'docs:     Documentation only changes（文档修改）'
     },
     {
-      value: 'style',
+      value: ':art: style',
       name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)（代码格式修改，注意不是css修改）'
     },
     {
-      value: 'refactor',
+      value: ':lipstick: UI',
+      name: 'UI: Update UI or styles file(css,less,scss)'
+    },
+    {
+      value: ':hammer: refactor',
       name: 'refactor: A code change that neither fixes a bug nor adds a feature（代码重构）'
     },
     {
-      value: ':rocket: perf',
+      value: ':zap: perf',
       name: 'perf:     A code change that improves performance（更改代码以提高性能）'
     },
     {
-      value: '🍉test',
-      name: '🍉test:     Adding missing tests（测试用例修改）'
+      value: ':white_check_mark: test',
+      name: 'test:     Adding missing tests（测试用例修改）'
     },
     {
-      value: '🍌chore',
-      name: '🍌chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation（其他修改，比如构建流程，依赖管理）'
+      value: ':rocket: chore',
+      name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation（其他修改，比如构建流程，依赖管理）'
     },
     {
-      value: '🍓revert',
-      name: '🍓revert:   Revert to a commit（恢复为提交）'
+      value: ':rewind: revert',
+      name: 'revert:   Revert to a commit（恢复为提交）'
     },
     {
-      value: '🍇WIP',
-      name: '🍇WIP:      Work in progress（Work in progress）'
+      value: ':fire: remove',
+      name: 'remove: Remove code or file'
+    }, {
+      value: ':construction: WIP',
+      name: 'WIP:      Work in progress（Work in progress）'
     }
   ],
 
@@ -59,21 +66,13 @@ module.exports = {
       name: 'component'
     },
     {
-      name: 'utils'
-    },
-    {
       name: 'build'
     }, {
       name: 'service'
-
     }, {
       name: 'core'
     }, {
       name: 'feature'
-    }, {
-      name: 'style'
-    }, {
-      name: 'docs'
     }, {
       name: 'other'
     }
